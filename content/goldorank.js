6f88cdf096c7d1bcbd98fe0537f5179def5c1c5e
henri <henri@d22a5453-2a0d-0410-a008-ae7450882971>
2006-02-24 16:21:06 +0000

    selection des moteurs et recherche
    git-svn-id: http://ganesha.websiteburo.com/svn/goldorank@8 d22a5453-2a0d-0410-a008-ae7450882971

---
+++ content/goldorank.js
@@ -0,0 +1,45 @@
+function peupleValeurs(){
+    document.getElementById('motscles').value=opener.content.document.getSelection();
+    document.getElementById('page').value=opener.content.document.location;
+}
+
+function rechercher() {
+    var searchText = document.getElementById('motscles').value;
+    var engine = document.getElementById("searchEngineList").value;
+    var searchURL;
+    
+    var searchSvc = Components.classes["@mozilla.org/rdf/datasource;1?name=internetsearch"].getService(Components.interfaces.nsIInternetSearchService);
+
+    // XXX Bug 269994: Use dummy string if there is no user entered string
+    searchURL = searchSvc.GetInternetSearchURL(engine, searchText ? encodeURIComponent(searchText):"A", 0, 0, {value:0});
+    
+    try {
+      // Get the engine's base URL
+      searchURL = makeURI(searchURL).host;
+    } catch (ex) {}
+    
+    window.opener.delayedSearchLoadURL(searchURL);
+    
+    //window.open('http://www.google.com/search?q='+document.getElementById('motscles').value, '', 'chrome,minimizable,resizable')
+}
+
+/* fonctions tirees de browser.js 
+function delayedSearchLoadURL(aURL) {
+  setTimeout(SearchLoadURL, 0, aURL);
+}
+
+function SearchLoadURL(aURL) {
+    if (!aURL) return;
+    loadURI(aURL, null, null);
+    content.focus();
+}
+
+function loadURI(uri, referrer, postData)
+{
+  try {
+    if (postData === undefined)
+      postData = null;
+    getWebNavigation().loadURI(uri, nsIWebNavigation.LOAD_FLAGS_NONE, referrer, postData, null);
+  } catch (e) {
+  }
+}*/
