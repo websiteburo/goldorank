aad1e62bd9024a867c3240b107c4780d5b8c8634
henri <henri@d22a5453-2a0d-0410-a008-ae7450882971>
2006-03-02 16:44:13 +0000

    recup results ok
    git-svn-id: http://ganesha.websiteburo.com/svn/goldorank@11 d22a5453-2a0d-0410-a008-ae7450882971

--- content/goldorank.js
+++ content/goldorank.js
@@ -13,16 +13,34 @@ function rechercher() {
     // XXX Bug 269994: Use dummy string if there is no user entered string
     searchURL = searchSvc.GetInternetSearchURL(engine, searchText ? encodeURIComponent(searchText):"A", 0, 0, {value:0});
     
+    if (searchSvc.FindInternetSearchResults(searchURL)){
+        var rdfService = Components.classes["@mozilla.org/rdf/rdf-service;1"].getService(Components.interfaces.nsIRDFService);
+        var ds = rdfService.GetDataSource('rdf:internetsearch');
+        
+        var t='';
+        var all_res=ds.GetAllResources();
+        regexEngine = /^engine/
+        for(var r=0; all_res.hasMoreElements(); r++)  {
+            var res=all_res.getNext();
+            if (res instanceof Components.interfaces.nsIRDFResource && !res.Value.match(regexEngine))    {
+                t += res.Value + "\n";
+            }
+        }
+        alert(t);
+    }
+    
+    /*
     try {
       // Get the engine's base URL
       searchURL = makeURI(searchURL).host;
     } catch (ex) {}
     
     var resultsTree = document.getElementById( "resultsList" );
-    resultsTree.setAttribute("ref", decodeURI(searchURL));
-    //window.opener.delayedSearchLoadURL(searchURL);
+resultsTree.setAttribute("ref", decodeURI(searchURL));
+    window.opener.delayedSearchLoadURL(searchURL);
     
-    //window.open('http://www.google.com/search?q='+document.getElementById('motscles').value, '', 'chrome,minimizable,resizable')
+    window.open('http://www.google.com/search?q='+document.getElementById('motscles').value, '', 'chrome,minimizable,resizable');
+    */
 }
 
 /* fonctions tirees de browser.js 
