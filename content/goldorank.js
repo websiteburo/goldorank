708d2d3d5b4ebe45f4ec6ff12b0d6e706404fb5c
henri <henri@d22a5453-2a0d-0410-a008-ae7450882971>
2006-03-04 11:22:19 +0000

    ecup resultats ok
    git-svn-id: http://ganesha.websiteburo.com/svn/goldorank@14 d22a5453-2a0d-0410-a008-ae7450882971

--- content/goldorank.js
+++ content/goldorank.js
@@ -3,66 +3,55 @@ function peupleValeurs(){
     document.getElementById('page').value=opener.content.document.location;
 }
 
-function afficheResults(){
-    var rdfService = Components.classes["@mozilla.org/rdf/rdf-service;1"].getService(Components.interfaces.nsIRDFService);
-    var ds = rdfService.GetDataSource('rdf:internetsearch');
+function afficheResults(engine){
     
-    
-    //*
-    var observer = {
-        onBeginLoad : function(sink){},
-        onInterrupt : function(sink){},
-        onResume : function(sink){},
-        onError : function(sink,status,msg){},
-        onEndLoad : function(sink){
-            sink.removeXMLSinkObserver(this);
-            sink.QueryInterface(Components.interfaces.nsIRDFDataSource);
-            
-            var tr='';
-            var searchroot = rdfService.GetResource('NC:LastSearchRoot');
-            var predicat = rdfService.GetResource('http://home.netscape.com/NC-rdf#child');
-            var rdf_url = rdfService.GetResource('http://home.netscape.com/NC-rdf#URL');
-            var resultats = ds.GetTargets(searchroot, predicat, true);
-            while (resultats.hasMoreElements()) {
-                var resultat = resultats.getNext();
-                var url = ds.GetTarget(resultat, rdf_url, true);
-                if (url) url = url.QueryInterface(Components.interfaces.nsIRDFLiteral);
-                if (url) tr += url.Value + "\n";            
-            }
-            alert(tr);
-        }
-    };
-
-    //var ds=rdfService.GetDataSource("http://www.xulplanet.com/tutorials/xultu/animals.rdf");
-    ds.QueryInterface(Components.interfaces.nsIRDFXMLSink);
-    ds.addXMLSinkObserver(observer);
-//*/
 }
 
 function rechercher() {
     var searchText = document.getElementById('motscles').value;
     var engine = document.getElementById("searchEngineList").value;
-    var searchURL;
     
     var searchSvc = Components.classes["@mozilla.org/rdf/datasource;1?name=internetsearch"].getService(Components.interfaces.nsIInternetSearchService);
 
     // XXX Bug 269994: Use dummy string if there is no user entered string
-    searchURL = searchSvc.GetInternetSearchURL(engine, searchText ? encodeURIComponent(searchText):"A", 0, 0, {value:0});
+    var searchURL = searchSvc.GetInternetSearchURL(engine, searchText ? encodeURIComponent(searchText):"A", 0, 0, {value:0});
     
-    if (searchSvc.FindInternetSearchResults(searchURL)){
-        afficheResults()
-        /*
-        var all_res=ds.GetAllResources();
-        regexEngine = /^engine/
-        for(var r=0; all_res.hasMoreElements(); r++)  {
-            var res=all_res.getNext();
-            //if (res instanceof Components.interfaces.nsIRDFResource && !res.Value.match(regexEngine))    {
-            if (res instanceof Components.interfaces.nsIRDFResource)    {
-                t += r + " " + res.Value + "\n";
-            }
-        }
-        //*/
-        
+    var rdfService = Components.classes["@mozilla.org/rdf/rdf-service;1"].getService(Components.interfaces.nsIRDFService);
+    var ds = rdfService.GetDataSource('rdf:internetsearch');
+    var searchroot = rdfService.GetResource('NC:LastSearchRoot');
+    var predicat = rdfService.GetResource('http://home.netscape.com/NC-rdf#child');
+    var rdf_url = rdfService.GetResource('http://home.netscape.com/NC-rdf#URL');
+    var rdf_data = rdfService.GetResource('http://home.netscape.com/NC-rdf#data');
+    var listeResultats = [];
+    
+    //Détermination du nombre de résultats par page
+    var txtEngine = ds.GetTarget(rdfService.GetResource(engine), rdf_data, true);
+    if (txtEngine) txtEngine = txtEngine.QueryInterface(Components.interfaces.nsIRDFLiteral);
+    if (txtEngine) txtEngine = txtEngine.Value;
+    var nbRes = 7;
+    regexNbRes = /factor=["']?(\d+)["']?/
+    rechNbRes = txtEngine.match(regexNbRes);
+    if (rechNbRes != null){
+        nbRes = rechNbRes[1];
     }
     
+    var RDF_observer = {
+        onAssert : function(ds, src, prop, target)  {        
+            if (prop == rdf_url) {
+                url = target.QueryInterface(Components.interfaces.nsIRDFLiteral);
+                listeResultats.push(url.Value);
+                if (listeResultats.length == nbRes){
+                    alert(listeResultats.toString());
+                    ds.RemoveObserver(this);
+                }
+            }
+        },
+        onUnassert : function(ds, src, prop, target){},
+        onChange : function(ds, src, prop, old_target, new_target) {},
+        onMove : function(ds, old_src, new_src, prop, target){},
+        onBeginUpdateBatch : function(ds){},
+        onEndUpdateBatch   : function(ds){}
+    }
+    ds.AddObserver(RDF_observer);
+    searchSvc.FindInternetSearchResults(searchURL);    
 }
