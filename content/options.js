ca986ac1158a205279a04ecd20f1f521fb53be48
henri <henri@d22a5453-2a0d-0410-a008-ae7450882971>
2006-11-04 21:08:13 +0000

    meilleur affichage options
    git-svn-id: http://ganesha.websiteburo.com/svn/goldorank@91 d22a5453-2a0d-0410-a008-ae7450882971

--- content/options.js
+++ content/options.js
@@ -3,3 +3,14 @@ function peupleValeurs(){
 	nodeListeMoteurs.datasources = fichierMoteurs;
 	nodeListeMoteurs.ref = "urn:goldorank:moteurs";
 }
+
+function getSelectedEngine(tree){
+	if (tree instanceof Components.interfaces.nsIXULTreeBuilder) {
+		alert('kkk');
+		desc_moteur = tree.getResourceAtIndex(tree.currentIndex);
+		if (desc_moteur instanceof Components.interfaces.nsIRDFResource){
+			strNom = ds_moteurs.GetTarget(desc_moteur, rdf_nom, true);
+			alert(strNom);
+		}
+	}
+}
