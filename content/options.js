4f767230c018b7f70ab259a57d8e888b3ed838d0
henri <henri@d22a5453-2a0d-0410-a008-ae7450882971>
2006-11-07 17:32:34 +0000

    getResourceAtIndex dans le tree options ok
    git-svn-id: http://ganesha.websiteburo.com/svn/goldorank@93 d22a5453-2a0d-0410-a008-ae7450882971

--- content/options.js
+++ content/options.js
@@ -5,12 +5,17 @@ function peupleValeurs(){
 }
 
 function getSelectedEngine(tree){
-	//~ if (tree.view instanceof Components.interfaces.nsIXULTreeBuilder) {
-	if (tree.view instanceof Components.interfaces.nsITreeView) {
-		desc_moteur = tree.view.getResourceAtIndex(tree.currentIndex);
-		if (desc_moteur instanceof Components.interfaces.nsIRDFResource){
-			strNom = ds_moteurs.GetTarget(desc_moteur, rdf_nom, true);
-			alert(strNom);
-		}
-	}
+	if (tree.view instanceof Components.interfaces.nsIXULTreeBuilder) {
+        desc_moteur = tree.view.getResourceAtIndex(tree.currentIndex);
+        if (desc_moteur instanceof Components.interfaces.nsIRDFResource){
+            strNom = ds_moteurs.GetTarget(desc_moteur, rdf_nom, true);
+            if (strNom) {
+                strNom = strNom.QueryInterface(Components.interfaces.nsIRDFLiteral);
+                if (strNom) {
+                    valeur = strNom.Value;
+                }
+            }
+            alert(valeur);
+        }
+    }
 }
