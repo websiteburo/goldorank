a8a6cf01952973fad60f36d28ffb99600a04898a
henri <henri@d22a5453-2a0d-0410-a008-ae7450882971>
2006-11-13 20:54:34 +0000

    options affichage selectionne
    git-svn-id: http://ganesha.websiteburo.com/svn/goldorank@95 d22a5453-2a0d-0410-a008-ae7450882971

--- content/options.js
+++ content/options.js
@@ -4,18 +4,30 @@ function peupleValeurs(){
 	nodeListeMoteurs.ref = "urn:goldorank:moteurs";
 }
 
+function getMoteurProperty(dsmoteur, prop){
+    var valeur = "";
+    var rdf_prop = rdfService.GetResource('urn:goldorank:rdf#'+prop);
+    var txtProp = ds_moteurs.GetTarget(dsmoteur, rdf_prop, true);
+    if (txtProp) {
+        txtProp = txtProp.QueryInterface(Components.interfaces.nsIRDFLiteral);
+        if (txtProp) {
+            valeur = txtProp.Value;
+        }
+    }
+    return valeur;
+}
+
 function getSelectedEngine(tree){
 	if (tree.view instanceof Components.interfaces.nsIXULTreeBuilder) {
         desc_moteur = tree.view.getResourceAtIndex(tree.currentIndex);
         if (desc_moteur instanceof Components.interfaces.nsIRDFResource){
-            strNom = ds_moteurs.GetTarget(desc_moteur, rdf_nom, true);
-            if (strNom) {
-                strNom = strNom.QueryInterface(Components.interfaces.nsIRDFLiteral);
-                if (strNom) {
-                    valeur = strNom.Value;
-                }
-            }
-            alert(valeur);
-        }
+			proprietesMoteur = new Array('nom', 'serveur', 'url');
+			for a in proprietesMoteur {
+				prop = proprietesMoteur[a];
+				alert(prop);
+				document.getElementById(prop).value = getMoteurProperty(desc_moteur, prop);
+			}
+		}
     }
 }
+
