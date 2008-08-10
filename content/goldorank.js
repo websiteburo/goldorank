9884c0071ed95339e2a9eea0d15ee2b9afcc60ce
henri <henri@d22a5453-2a0d-0410-a008-ae7450882971>
2008-08-10 20:07:27 +0000

    copie conforme pcperso
    git-svn-id: http://ganesha.websiteburo.com/svn/goldorank@119 d22a5453-2a0d-0410-a008-ae7450882971

--- content/goldorank.js
+++ content/goldorank.js
@@ -6,13 +6,110 @@ nodeButton.setAttribute('oncommand', 'interruptionMoteur();');
 function peupleValeurs(){
     document.getElementById('motscles').value=opener.content.document.getSelection();
     document.getElementById('page').value=opener.content.document.location;
-    
-    nodeTabPanel = document.getElementById("tabpanel_modele");
-    nodeRow = document.getElementById("row_modele");
+
+    // Construction des modeles
+    stringsBundle = document.getElementById("string-bundle");
+    //.....NodeTabPanel
+    nodeTabPanel = document.createElement('tabpanel');
+    nodeTabPanel.setAttribute('flex', '1');
+    nodeGrid = document.createElement('grid');
+    nodeGrid.setAttribute('flex', '1');
+    nodeGrid.setAttribute('class', 'simililist');
+    nodeColumns = document.createElement('columns');
+    nodeColumn = document.createElement('column');
+    nodeColumn1 = document.createElement('column');
+    nodeColumn1.setAttribute('flex', '1');
+    nodeColumn2 = document.createElement('column');
+    nodeColumn2.setAttribute('flex', '2');
+    nodeColumn3 = document.createElement('column');
+    nodeColumn3.setAttribute('flex', '1');
+    nodeColumn4 = document.createElement('column');
+    nodeColumn4.setAttribute('flex', '1');
+    nodeColumn5 = document.createElement('column');
+    nodeColumn5.setAttribute('flex', '2');
+    nodeColumns.appendChild(nodeColumn);
+    nodeColumns.appendChild(nodeColumn1);
+    nodeColumns.appendChild(nodeColumn2);
+    nodeColumns.appendChild(nodeColumn3);
+    nodeColumns.appendChild(nodeColumn4);
+    nodeColumns.appendChild(nodeColumn5);
+    nodeRows = document.createElement('rows');
+    nodeRoww = document.createElement('row');
+    nodeRoww.setAttribute('class', 'simililistHeader');
+    nodeLabel = document.createElement('label');
+    nodeLabel.setAttribute('value', ' ');
+    nodeLabel1 = document.createElement('label');
+    nodeLabel1.setAttribute('value', ''+stringsBundle.getString('stEngine'));
+    nodeLabel2 = document.createElement('label');
+    //nodeLabel2.setAttribute('value', stringsBundle.getString('stProgress');
+    nodeLabel3 = document.createElement('label');
+    //nodeLabel3.setAttribute('value', stringsBundle.getString('stRank'));
+    nodeLabel4 = document.createElement('label');
+    //nodeLabel4.setAttribute('value', stringsBundle.getString('stPage'));
+    nodeLabel5 = document.createElement('label');
+    //nodeLabel5.setAttribute('value',stringsBundle.getString('stResults'));
+    nodeRoww.appendChild(nodeLabel);
+    nodeRoww.appendChild(nodeLabel1);
+    nodeRoww.appendChild(nodeLabel2);
+    nodeRoww.appendChild(nodeLabel3);
+    nodeRoww.appendChild(nodeLabel4);
+    nodeRoww.appendChild(nodeLabel5);
+    nodeRows.appendChild(nodeRoww);
+    nodeGrid.appendChild(nodeColumns);
+    nodeGrid.appendChild(nodeRows);
+    nodeTabPanel.appendChild(nodeGrid);
+
+    //........nodeRow
+    nodeRow = document.createElement('row');
+    nodeHbox = document.createElement('hbox');
+    nodeCheckbox = document.createElement('checkbox');
+    nodeCheckbox.setAttribute('checked', 'false');
+    nodeHbox.appendChild(nodeCheckbox);
+    nodeHbox1 = document.createElement('hbox');
+    nodeVbox = document.createElement('vbox');
+    nodeSpacer = document.createElement('spacer');
+    nodeSpacer.setAttribute('flex', '1');
+    nodeImage = document.createElement('image');
+    nodeImage.setAttribute('class', '.engine-icon');
+    nodeSpacer1 = document.createElement('spacer');
+    nodeSpacer1.setAttribute('flex', '1');
+    nodeLabelr = document.createElement('label');
+    nodeVbox.appendChild(nodeSpacer);
+    nodeVbox.appendChild(nodeImage);
+    nodeVbox.appendChild(nodeSpacer1);
+    nodeHbox1.appendChild(nodeVbox);
+    nodeHbox1.appendChild(nodeLabelr);
+    nodeHbox2 = document.createElement('hbox');
+    nodeProgress = document.createElement('progressmeter');
+    nodeProgress.setAttribute('mode', 'determined');
+    nodeProgress.setAttribute('value', '0');
+    nodeProgress.setAttribute('flex', '1');
+    nodeHbox2.appendChild(nodeProgress);
+    nodeHbox3 = document.createElement('hbox');
+    nodeLabelr1 = document.createElement('label');
+    nodeHbox3.appendChild(nodeLabelr1);
+    nodeHbox4 = document.createElement('hbox');
+    nodeLabelr2 = document.createElement('label');
+    nodeHbox4.appendChild(nodeLabelr2);
+    nodeHbox5 = document.createElement('hbox');
+    nodeMenulist = document.createElement('menulist');
+    nodeMenulist.setAttribute('oncommand', 'ouvreUrl(this.value);');
+    nodeMenulist.setAttribute('style', 'display:none;');
+    nodeMenupopup = document.createElement('menupopup');
+    nodeMenulist.appendChild(nodeMenupopup);
+    nodeHbox5.appendChild(nodeMenulist);
+    nodeRow.appendChild(nodeHbox);
+    nodeRow.appendChild(nodeHbox1);
+    nodeRow.appendChild(nodeHbox2);
+    nodeRow.appendChild(nodeHbox3);
+    nodeRow.appendChild(nodeHbox4);
+    nodeRow.appendChild(nodeHbox5);
+    // Fin construction modeles
     
     nodeTabs = document.getElementById('lestabs');
     nodeTabPanels = document.getElementById('lestabpanels');
     
+   
     // Construction des tabs et des moteurs à partir du fichier rdf
     container.Init(ds_moteurs, rdf_moteurs);
     var moteurs_langues = container.GetElements();
@@ -57,9 +154,11 @@ function peupleValeurs(){
             unrow.firstChild.nextSibling.firstChild.firstChild.nextSibling.setAttribute('src', strLogo);
             unrow.firstChild.nextSibling.firstChild.nextSibling.setAttribute('value', strNom);
             untabpanel.firstChild.firstChild.nextSibling.appendChild(unrow);
+            //unrow.setAttribute('style', 'display:inline;');
         }
         
         nodeTabPanels.appendChild(untabpanel);
+        //untabpanel.setAttribute('style', 'display:inline;');
       }
     }
     nodeTabs.firstChild.selected='true';
@@ -91,7 +190,7 @@ function rechercherS(){
 			while (resultsCell.firstChild){
 				resultsCell.removeChild(resultsCell.firstChild);
 			}
-		resultsCell.parentNode.setAttribute('style', 'display:none;');
+		//resultsCell.parentNode.setAttribute('style', 'display:none;');
 		//Bouton stop
 		if (resultsCell.parentNode.parentNode.length == 2){
 			resultsCell.parentNode.parentNode.removeChild(nodeButton);
