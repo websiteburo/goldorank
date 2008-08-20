421a73e3c9fe6998f11d6d5320272843aca99ef1
henri <henri@d22a5453-2a0d-0410-a008-ae7450882971>
2008-08-20 08:39:19 +0000

    utilisation de spock pour signer update.rdf
    git-svn-id: http://ganesha.websiteburo.com/svn/goldorank@122 d22a5453-2a0d-0410-a008-ae7450882971

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# coding=utf-8
+#-*- coding: utf-8 -*-
 
 app = 'goldorank'
 name = 'Goldorank'
@@ -7,13 +7,15 @@ description = 'Search engines position retriever by WebSiteBuro'
 author = 'Henri Bourcereau'
 authorURL = "http://www.websiteburo.com"
 uid = 'e4b43b01-1ccb-476a-92d2-9a193ad4ff50'
-#clé publique de 'goldorank' générée par /usr/local/mccoy/mccoy 
-updateKey = 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDqA8cSkYutSOSs/PoxLBe/KWMNUmq7snZwKxLvaoZCcSn19LZusq9LKnQrsrawB+aLzCXX2U6i1xA0owmf1VSdC0xZQhzecdHBribyHAPkEu8wx0fy1ADCxxXJ52xkRNi8pOVOuI9nX9DN9GuL3M/zCfIftgCb3fkEmVqIZvcISQIDAQAB'
+#clé publique de 'goldorank' générée par mccoy (sans mot de passe pour utilisation par spock)
+updateKey = 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCZQlUWmH3uuZ6UE4QswRRjGoiXJwdWB+U072dnBXrZB/533kYwRJwZYBzIozayvUrvxojQDa7ZjJ47o8qPJ57F/+b0YM5kcoppVJZ6U0sEbxL+8X8Cgn8TTg7jbaKQyzr+/uqxYebUzPnr9MEYf2mW2j1UFOZ5A6Nbj78GyLEg4wIDAQAB'
+nssDirectory = '/home/henri/.mozilla/mccoy/2osghvae.default/'
 
 major_version = 0
 minor_version = 8
-build_version = 2
+build_version = 3
 in_development = False
+whatsnew = '<ul><li>Compatible with Firefox 3</li><li>Some corrections in search engines definitions</li></ul>'
 
 version = "%d.%d.%d%s" % (
     major_version,
@@ -25,6 +27,7 @@ version = "%d.%d.%d%s" % (
 
 homepageURL = "http://www.websiteburo.com/%(app)s" % vars()
 updateURL = "%(homepageURL)s/update.rdf" % vars()
+updateInfoFile = "updateinfo%(version)s.xhtml" % vars() 
 updateFile = "%(app)s-%(version)s.xpi" % vars()
 updateLink = "%(homepageURL)s/%(updateFile)s" % vars()
 firefoxUID = 'ec8030f7-c20a-464f-9b0e-13a3a9e97384'
